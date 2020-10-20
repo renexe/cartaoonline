cc0661a07f9ceefe444121fdd736ae316f1c42f2
Rene <rene.exe@gmail.com>
2020-10-20 11:16:28 -0300

    Criando Homepage
    #Close24

--- conline/base/tests/base/test_home.py
+++ conline/base/tests/base/test_home.py
@@ -1,31 +1,26 @@
-# import pytest
-# from django.test import Client
-# from django.urls import reverse
+import pytest
+from django.urls import reverse
 
-# from conline.django_assertions import assert_contains
-
-
-def test_test():
-    return 1
+from conline.django_assertions import assert_contains
 
 
-# @pytest.fixture
-# def resp(client, db):
-#     resp = client.get(reverse('base:home'))
-#     return resp
-#
-#
-# def test_status_code(resp):
-#     resp.status_code == 200
-#
-#
-# def test_title(resp):
-#     assert_contains(resp, '<title>Python Pro - Home</title>')
-#
-#
-# def test_home_link(resp):
-#     assert_contains(resp, f'href="{reverse("base:home")}">Python Pró</a>')
-#
-#
-# def test_email_link(resp):
-#     assert_contains(resp, 'href="mailto:rene.exe@gmail.com"')
+@pytest.fixture
+def resp(client, db):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resp):
+    resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Procure um cartãO Online</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}">cartãOOnline</a>')
+
+
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:renelima.dev@gmail.com"')
